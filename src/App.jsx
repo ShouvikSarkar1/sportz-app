8e8e2d72ffd3f04bae73d1486d1a33b88c3ddfde
jagui1 <agui1@umbc.edu>
2025-03-11 17:05:13 -0400

    Adds backend python functionality using Flask and axios to call placeholder backend logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,39 @@ import './App.css'
 import TeamSelect from './components/TeamSelect/TeamSelect'
 import TeamLogo from './components/TeamLogo/TeamLogo'
 import { useState } from 'react'
+import axios from "axios";
 
 function App() {
   const [team, setTeam] = useState(null)
+  const [prediction, setPrediction] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/predict", {teamName: team});
+      setPrediction(response.data.result);  
+
+    } catch (error) {
+      console.error("Error making prediction", error);
+    }
+  }
 
   return (
-    <>
-      <div className='TeamLogo'>
-        <TeamLogo team={team} />
-      </div>
-      <div className='Select'>
-        <TeamSelect onTeamSelect={setTeam}/>
-      </div>
-    </>
-  )
+    <div>
+      <h1>Select an NFL team</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='TeamLogo'>
+          <TeamLogo team={team} />
+        </div>
+        <div className='Select'>
+          <TeamSelect onTeamSelect={setTeam}/>
+        </div>
+        <button type="submit">Predict</button>
+      </form>
+      {prediction && <p>Prediction: {prediction}</p>}
+    </div>
+  );
 }
 
 export default App
